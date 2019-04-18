904c6e01aa0800645b6ef90636c851fe9e0db944
jadenswang <jaden.shiteng.wang@gmail.com>
2019-04-18 15:51:29 -0400

    Changed to Class System

--- javascript/canvasGeometry.js
+++ javascript/canvasGeometry.js
@@ -12,29 +12,138 @@ function animate(time) {
 	requestAnimationFrame(animate);
 }
 
-// basic "cursor" movement
-function moveLeft(blockMesh) {
-	blockMesh.position.set(blockMesh.position.x - 10, blockMesh.position.y, blockMesh.position.z);
+class Block {
+	_blockMesh;
+	_center;
+
+	constructor(block, material, center, rotation) {
+		// creates the "cursor"
+		this._blockMesh = new THREE.Mesh(block, material);
+		scene.add(this._blockMesh);
+
+		// set the location of the block
+		this._blockMesh.position.set(center.x, center.y, center.z);
+		this._blockMesh.rotation.set(rotation.x, rotation.y, rotation.z);
+
+		this._center = center;
+	}
+
+	/**
+	 * TO-DO
+	 */
+	delete = function () {
+		var selectedObject = scene.getObjectByName(this._blockMesh.name);
+		scene.remove(selectedObject);
+		animate();
+	}
+
+	getMesh = function () {
+		return this._blockMesh;
+	}
+
+	getRotation = function () {
+		return this._blockMesh.rotation;
+	}
 }
 
-function moveRight(blockMesh) {
-	blockMesh.position.set(blockMesh.position.x + 10, blockMesh.position.y,	blockMesh.position.z);
+class CursorBlock extends Block {
+	constructor(block, material) {
+		super(block, material, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0));
+	}
+
+	// basic cursor movement
+	moveLeft = function () {
+		this._blockMesh.position.set(this._blockMesh.position.x - 10, this._blockMesh.position.y, this._blockMesh.position.z);
+	}
+
+	moveRight = function () {
+		this._blockMesh.position.set(this._blockMesh.position.x + 10, this._blockMesh.position.y, this._blockMesh.position.z);
+	}
+
+	moveDown = function () {
+		this._blockMesh.position.set(this._blockMesh.position.x, this._blockMesh.position.y - 10, this._blockMesh.position.z);
+	}
+
+	moveUp = function () {
+		this._blockMesh.position.set(this._blockMesh.position.x, this._blockMesh.position.y + 10, this._blockMesh.position.z);
+	}
+
+	moveOut = function () {
+		this._blockMesh.position.set(this._blockMesh.position.x, this._blockMesh.position.y, this._blockMesh.position.z + 10);
+	}
+
+	moveIn = function () {
+		this._blockMesh.position.set(this._blockMesh.position.x, this._blockMesh.position.y, this._blockMesh.position.z - 10);
+	}
 }
 
-function moveDown(blockMesh) {
-	blockMesh.position.set(blockMesh.position.x, blockMesh.position.y - 10,	blockMesh.position.z);
-}
+class GeometricObjects {
+	_placedObjectMeshes;
+	_cursor;
 
-function moveUp(blockMesh) {
-	blockMesh.position.set(blockMesh.position.x, blockMesh.position.y + 10,	blockMesh.position.z);
-}
+	constructor() {
+		this._cursor = new CursorBlock(
+			new THREE.BoxGeometry(10, 10, 10),
+			new THREE.MeshPhongMaterial({
+				color: 0xFFFFFF,
+				specular: 0xffffff,
+				shininess: 1000
+			}),
+		);
 
-function moveOut(blockMesh) {
-	blockMesh.position.set(blockMesh.position.x, blockMesh.position.y,	blockMesh.position.z + 10);
-}
+		this._placedObjectMeshes = [];
+	}
 
-function moveIn(blockMesh) {
-	blockMesh.position.set(blockMesh.position.x, blockMesh.position.y,	blockMesh.position.z - 10);
+	// drawing shapes
+	drawCube = function (color) {
+		const box = new THREE.Box3().setFromObject(this._cursor.getMesh());
+		var block = new Block(
+			new THREE.BoxGeometry(box.getSize().x, box.getSize().y, box.getSize().z),
+			new THREE.MeshPhongMaterial({
+				color: color,
+				specular: 0xffffff,
+				shininess: 1000
+			}),
+			box.getCenter(),
+			this._cursor.getRotation());
+
+		// Add to the array of all the objects
+		this._placedObjectMeshes.push(block);
+	}
+
+	drawSphere = function (color) {
+		const box = new THREE.Box3().setFromObject(this._cursor.getMesh());
+		var block = new Block(
+			new THREE.SphereGeometry(box.getSize().x / 2, 10, 10),
+			new THREE.MeshPhongMaterial({
+				color: color,
+				specular: 0xffffff,
+				shininess: 1000
+			}),
+			box.getCenter(),
+			this._cursor.getRotation()
+		);
+
+		// Add to the array of all the objects
+		this._placedObjectMeshes.push(block);
+	}
+
+	drawCylinder = function (color) {
+		const box = new THREE.Box3().setFromObject(this._cursor.getMesh());
+		var block = new Block(
+			new THREE.CylinderGeometry(box.getSize().z / 2, box.getSize().z / 2, box.getSize().y),
+			new THREE.MeshPhongMaterial({
+				color: color,
+				specular: 0xffffff,
+				shininess: 1000
+			}),
+			box.getCenter(),
+			this._cursor.getRotation()
+		);
+
+		// Add to the array of all the objects
+		this._placedObjectMeshes.push(block);
+	}
 }
 
 // // block resizing
@@ -45,55 +154,6 @@ function moveIn(blockMesh) {
 // function resizeBlockDown(block) {
 // 	block.size.set(block.width, block.height, block.depth);
 // }
-
-// drawing shapes
-function drawCube(color) {
-	const box = new THREE.Box3().setFromObject(cursorMesh);
-	var block = new THREE.BoxGeometry(box.getSize().x, box.getSize().y, box.getSize().z);
-	var material = new THREE.MeshPhongMaterial({
-		color: color,
-		specular: 0xffffff,
-		shininess: 1000
-	});
-
-	addObject(block, material, box.getCenter());
-}
-
-function drawSphere(color) {
-	const box = new THREE.Box3().setFromObject(cursorMesh);
-	var block = new THREE.SphereGeometry(box.getSize().x/2, 10, 10);
-	var material = new THREE.MeshPhongMaterial({
-		color: color,
-		specular: 0xffffff,
-		shininess: 1000
-	});
-
-	addObject(block, material, box.getCenter());
-}
-
-function drawCylinder(color) {
-	const box = new THREE.Box3().setFromObject(cursorMesh);
-	var block = new THREE.CylinderGeometry(box.getSize().z/2, box.getSize().z/2, box.getSize().y);
-	var material = new THREE.MeshPhongMaterial({
-		color: color,
-		specular: 0xffffff,
-		shininess: 1000
-	});
-
-	addObject(block, material, box.getCenter());
-}
-
-function addObject(block, material, location) {
-	// creates the "cursor"
-	var blockMesh = new THREE.Mesh(block, material);
-
-	// set the location of the block
-	blockMesh.position.set(location.x, location.y, location.z);
-	blockMesh.rotation.set(cursorMesh.rotation.x, cursorMesh.rotation.y, cursorMesh.rotation.z);
-
-	// add final mesh to the scene
-	scene.add(blockMesh);
-}
 
 // rendering
 function render() {
