e156007e6a5d1e58c0695a1280d0733c1b9d26ae
JadenSWang <jaden.shiteng.wang@gmail.com>
2019-07-03 11:30:21 -0400

    Add Data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-var server = app.listen(80, () => {
+var server = app.listen(8080, () => {
 	var host = server.address().address;
 	var port = server.address().port;
 
@@ -14,3 +14,21 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.static('./public'));
+
+app.get('/angles', (req, res) => {
+	var fromPython = [1,2,3,4,1,5,2,32,1]
+	if (req.query.experimentid == 1) {
+		// gets a result array back
+		res.json({
+			theta: fromPython
+		});
+	} else if (req.query.experimentid == 2) {
+		res.json({
+			theta: [2, 2, 2, 2, 2]
+		});
+	} else if (req.query.experimentid == 22) {
+		res.json({
+			name: "lafe"
+		});
+	}
+});
